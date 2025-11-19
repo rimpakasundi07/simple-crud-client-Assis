5566e491e2a3d80023dea00ac819fef3dbde8c76
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-19 22:26:06 +0530

    Add update form with user data pre-population

--- src/components/UpdateUser.jsx
+++ src/components/UpdateUser.jsx
@@ -4,9 +4,24 @@ import { useLoaderData } from "react-router";
 const UpdateUser = () => {
   const user = useLoaderData();
   console.log(user);
+
+  const handleUpdateUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
+  };
+
   return (
     <div>
       <h3>Edit a User</h3>
+      <form onSubmit={handleUpdateUser}>
+        <input type="text" name="name" id="" defaultValue={user.name} />
+        <br></br>
+        <input type="email" name="email" id="" defaultValue={user.email} />
+        <br></br>
+        <input type="submit" value="Update user" />
+      </form>
     </div>
   );
 };
